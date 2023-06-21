e2dea4c31d206b95d58cf482c2bb08ef5bdb9344
JonathanAguirre <jonathan621924@gmail.com>
2023-06-21 13:01:22 -0300

    iniciando con la preEntrega

---
+++ javascript/productos.js
@@ -0,0 +1,32 @@
+// ARRAYS DE OBEJTOS
+
+const productos = [{
+    id:1,
+    modelo:"Samsung Galaxy S23 Ultra 12 GB 253 GB",
+    color:"negro",
+    precio: 52000
+},
+{
+    id:2,
+    modelo:"Samsung Galaxy A23 Negro 128 GB 4 GB ram",
+    color:"negro",
+    precio: 14000 
+},
+{
+    id:3,
+    modelo:"Samsung S20 FE 5G 128 GB cloud red 6 GB RAM",
+    color:"rojo",
+    precio: 24000
+},
+{
+    id:4,
+    modelo:"Samsung S23 Ultra 5g 256 gb",
+    color:"negro",
+    precio: 60000
+},
+{
+    id:5,
+    modelo:"Samgung S21 FE 5G",
+    color:"gris",
+    precio: 52000
+}];
