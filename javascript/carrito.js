c51905e966d9e7227b02bd6ecb80f5ed56d3e880
JonathanAguirre <jonathan621924@gmail.com>
2023-07-04 13:23:59 -0300

    modifiacando funciones de agregar al carro

--- javascript/carrito.js
+++ javascript/carrito.js
@@ -3,48 +3,48 @@ let totalHtml = document.getElementById("total");
 let carro = JSON.parse(localStorage.getItem("carro")) || [];
 
 function calcularTotal() {
-    let total = carro.reduce((ac, prod) => {
-        if (typeof prod.precio === 'number' && !isNaN(prod.precio)) {
-            return ac + prod.precio;
-        } else {
-            return ac;
-        }
-    }, 0);
+  let total = carro.reduce((ac, prod) => {
+    if (typeof prod.precio === 'number' && !isNaN(prod.precio)) {
+      return ac + (prod.precio * prod.cantidad); 
+    } else {
+      return ac;
+    }
+  }, 0);
 
-    console.log(total);
-    totalHtml.innerHTML = `Total a pagar: $${total}`;
+  console.log(total);
+  totalHtml.innerHTML = `Total a pagar: $${total}`;
 
-    tablaCarrito.innerHTML = "";
+  tablaCarrito.innerHTML = "";
 
-    carro.forEach((prod, index) => {
-        const fila = document.createElement("tr");
-        const imagenCelda = document.createElement("td");
-        const imagen = document.createElement("img");
-        imagen.src = prod.foto;
-        imagen.alt = "Imagen del producto";
-        imagen.style.width = "70px";
-        imagen.style.height = "auto";
-        imagenCelda.appendChild(imagen);
-        fila.appendChild(imagenCelda);
-        fila.innerHTML += `
-            <td>${prod.modelo}</td>
-            <td class="fw-bold fs-4">$${prod.precio}</td>
-            <button id=${prod.id} class="eliminar">Eliminar Producto</button>
-        `;
-        tablaCarrito.appendChild(fila);
+  carro.forEach((prod, index) => {
+    const fila = document.createElement("tr");
+    const imagenCelda = document.createElement("td");
+    const imagen = document.createElement("img");
+    imagen.src = prod.foto;
+    imagen.alt = "Imagen del producto";
+    imagen.style.width = "70px";
+    imagen.style.height = "auto";
+    imagenCelda.appendChild(imagen);
+    fila.appendChild(imagenCelda);
+    fila.innerHTML += `
+      <td>${prod.modelo}</td>
+      <td class="fw-bold fs-4">$${prod.precio}</td>
+      <td class="fw-bold fs-4">${prod.cantidad}</td> 
+      <button id=${prod.id} class="eliminar">Eliminar Producto</button>
+    `;
+    tablaCarrito.appendChild(fila);
 
-        let botones = document.getElementsByClassName("eliminar");
-        for (const boton of botones) {
-            boton.addEventListener("click", () => {
-                const prodACarro = carro.find((producto) => producto.id == boton.id);
-                carro.splice(index, 1);
-                localStorage.setItem("carro", JSON.stringify(carro));
-                calcularTotal();
-                localStorage.removeItem("carro");
-                console.log(prodACarro);
-            });
-        }
-    });
+    let botones = document.getElementsByClassName("eliminar");
+    for (const boton of botones) {
+      boton.addEventListener("click", () => {
+        const prodACarro = carro.find((producto) => producto.id == boton.id);
+        carro.splice(index, 1);
+        localStorage.setItem("carro", JSON.stringify(carro));
+        calcularTotal();
+        console.log(prodACarro);
+      });
+    }
+  });
 }
 
 calcularTotal();
@@ -52,17 +52,17 @@ calcularTotal();
 //FINALIZAR COMPRA
 let finalizarBtn = document.getElementById("finalizar");
 finalizarBtn.onclick = () => {
-    carro = [];
-    document.getElementById("tablaCarrito").innerHTML = '';
-    document.getElementById("total").innerHTML = `El carrito esta vacio`;
-    localStorage.removeItem("carro");
-    Toastify({
-        text: 'Gracias por tu compra, en las proximas 48 horas recibiras tu pedido!',
-        duration: 3000,
-        gravity: 'botton',
-        position: 'left',
-        style: {
-            background: "linear-gradient(to right, #00b09b, #96c93d)",
-        },
-    }).showToast();
+  carro = [];
+  document.getElementById("tablaCarrito").innerHTML = '';
+  document.getElementById("total").innerHTML = `El carrito está vacío`;
+  localStorage.removeItem("carro");
+  Toastify({
+    text: 'Gracias por tu compra, en las próximas 48 horas recibirás tu pedido!',
+    duration: 3000,
+    gravity: 'bottom',
+    position: 'left',
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
 }
--- javascript/miArchivo.js
+++ javascript/miArchivo.js
@@ -3,66 +3,62 @@ const carroExistente = localStorage.getItem("carro");
 const carro = carroExistente ? JSON.parse(carroExistente) : [];
 
 function renderizarProductos(listaprods) {
-    //vaciamos el contenedor para evitar duplicados
-    contenedorProds.innerHTML = "";
-    //cargamos las cartas de los productos solicitados
-    for (const prod of listaprods) {
-        contenedorProds.innerHTML += `
-        <div class="card col-sm-3 m-2 bg-secondary border border-3 rounded-5 p-3" style="width: 250px">
-           <img class="card-img-top w-75 m-3" src=${prod.foto} alt="card image cap">
-            <div class="card-body d-grid ">
-            <h5 class="card-title text-center text-light fw-bolder">${prod.modelo}</h5>
-            <p class="card-text text-center text-light fw-bolder">$ ${prod.precio}</p>
-            <button id=${prod.id} class="btn btn-danger rounded-5 compra">Añadir al carrito</button>
-          </div>
-       </div>
-        `;
+  // Vaciamos el contenedor para evitar duplicados
+  contenedorProds.innerHTML = "";
+
+  // Cargamos las cartas de los productos solicitados
+  for (const prod of listaprods) {
+    contenedorProds.innerHTML += `
+      <div class="card col-sm-3 m-2 bg-secondary border border-3 rounded-5 p-3" style="width: 250px">
+        <img class="card-img-top w-75 m-3" src=${prod.foto} alt="card image cap">
+        <div class="card-body d-grid ">
+          <h5 class="card-title text-center text-light fw-bolder">${prod.modelo}</h5>
+          <p class="card-text text-center text-light fw-bolder">$ ${prod.precio}</p>
+          <button id=${prod.id} class="btn btn-danger rounded-5 compra">Añadir al carrito</button>
+        </div>
+      </div>
+    `;
+  }
+
+  // EVENTOS
+  let botones = document.getElementsByClassName("compra");
+  for (const boton of botones) {
+    boton.addEventListener("click", () => {
+      const prodACarro = productos.find((producto) => producto.id == boton.id);
+      console.log(prodACarro);
+
+      Swal.fire({
+        title: 'Fantastico!',
+        text: `Agregaste al carrito: ${prodACarro.modelo}`,
+        imageUrl: prodACarro.foto,
+        imageWidth: 250,
+        imageHeight: 150,
+        imageAlt: prodACarro.modelo,
+      })
+
+      const index = carro.findIndex((producto) => producto.id == prodACarro.id);
+
+      if (index !== -1) {
+        carro[index].cantidad++;
+      } else {
+        prodACarro.cantidad = 1;
+        carro.push(prodACarro);
+      }
+
+      localStorage.setItem("carro", JSON.stringify(carro));
+    });
+
+    boton.onmouseover = () => {
+      boton.classList.replace("btn-primary", "btn-warning");
     }
 
-    //EVENTOS
-    let botones = document.getElementsByClassName("compra");
-    for (const boton of botones) {
-        boton.addEventListener("click", () => {
-            const prodACarro = productos.find((producto) => producto.id == boton.id);
-            console.log(prodACarro);
-
-            Swal.fire({
-                title: 'Fantastico!',
-                text: `Agregaste al carrito: ${prodACarro.modelo}`,
-                imageUrl: prodACarro.foto,
-                imageWidth: 250,
-                imageHeight: 150,
-                imageAlt: prodACarro.modelo,
-            })
-
-            let carro;
-
-            if (carroExistente) {
-                carro = JSON.parse(carroExistente);
-                if (!Array.isArray(carro)) {
-                    carro = [];
-                }
-            } else {
-                carro = [];
-            }
-
-            carro.push(prodACarro);
-
-            localStorage.setItem("carro", JSON.stringify(carro));
-
-        })
-        boton.onmouseover = () => {
-            boton.classList.replace("btn-primary", "btn-warning");
-        }
-        boton.onmouseout = () => {
-            boton.classList.replace("btn-warning", "btn-primary");
-        }
+    boton.onmouseout = () => {
+      boton.classList.replace("btn-warning", "btn-primary");
     }
+  }
 }
 
-
 let tablaCarrito = document.getElementById("tablaCarrito");
-
 
 renderizarProductos(productos);
 
@@ -72,18 +68,19 @@ let minimo = document.getElementById("minimo");
 let maximo = document.getElementById("maximo");
 let marca = document.getElementById("modelo");
 
-//funcion para poder filtrar por precio
+// Función para filtrar por precio
 function filtrarPorPrecio(preciominimo, preciomaximo) {
-    const filtrados = productos.filter((prod) => (prod.precio >= preciominimo) && (prod.precio <= preciomaximo));
-    sessionStorage.setItem("filtrados", JSON.stringify(filtrados));
-    return filtrados;
+  const filtrados = productos.filter((prod) => (prod.precio >= preciominimo) && (prod.precio <= preciomaximo));
+  sessionStorage.setItem("filtrados", JSON.stringify(filtrados));
+  localStorage.setItem("filtrados", JSON.stringify(filtrados));
+  return filtrados;
 }
 
 filtro.onclick = () => {
-    if ((minimo.value != "") && (maximo.value != "") && (minimo.value < maximo.value)) {
-        let listaFiltrados = filtrarPorPrecio(minimo.value, maximo.value);
-        renderizarProductos(listaFiltrados);
-    }
+  if ((minimo.value != "") && (maximo.value != "") && (minimo.value < maximo.value)) {
+    let listaFiltrados = filtrarPorPrecio(minimo.value, maximo.value);
+    renderizarProductos(listaFiltrados);
+  }
 }
 
 //funcion para poder filtrar por marca
